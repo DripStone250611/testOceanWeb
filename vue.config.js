b8995859c7eb45a7dba943f206b7e9751180859b
YINAN <yinan@example.com>
2021-12-20 15:25:50 +0800

    add Dockerfile

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,11 @@
 module.exports = {
-    pwa: {
-        iconPaths: {
-            favicon32: 'favicon.ico',
-            favicon16: 'favicon.ico',
-            appleTouchIcon: 'favicon.ico',
-            maskIcon: 'favicon.ico',
-            msTileImage: 'favicon.ico'
-        }
-    },
+
     configureWebpack: {
         externals: {
             "BMap": "BMap"
-        }
-    }
+        },
+        mode: 'production'
+    },
+
 };
+
